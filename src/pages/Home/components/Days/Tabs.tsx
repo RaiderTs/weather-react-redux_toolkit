1cb482212af0fac4666cb88581804bf718fd4406
Alex Tsotsko <alex.tsotsko21@gmail.com>
2022-02-08 17:36:47 +0200

    feat: adds tabs

--- src/pages/Home/components/Days/Tabs.tsx
+++ src/pages/Home/components/Days/Tabs.tsx
@@ -1,10 +1,31 @@
 import React from "react";
-import s from './Days.module.scss'
+import s from "./Days.module.scss";
 
 type Props = {};
+export interface Tab {
+  value: string;
+}
 
 const Tabs = (props: Props) => {
-  return <div></div>;
+  const tabs = [
+    { value: "На неделю" },
+    { value: "На 10 дней" },
+    { value: "На месяц" },
+  ];
+  return (
+    <div className={s.tabs}>
+      <div className={s.tabs__wrapper}>
+        {tabs.map((tab: Tab) => (
+          <div key={tab.value} className={s.tab}>
+            {tab.value}
+          </div>
+        ))}
+      </div>
+      <button type="button" className={s.cancel}>
+        Отменить
+      </button>
+    </div>
+  );
 };
 
 export default Tabs;
