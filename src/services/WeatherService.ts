dbd5ce47a8b32f3b94e6dea2be16adf5c305638e
Alex Tsotsko <alex.tsotsko21@gmail.com>
2022-02-13 00:01:40 +0200

    feat: add axios

--- src/services/WeatherService.ts
+++ src/services/WeatherService.ts
@@ -1,12 +1,9 @@
-import axios, { AxiosResponse } from "axios";
+import { AxiosResponse } from "axios";
+import api from "../axios";
 import { Weather } from "../store/types/types";
-
-const API_KEY = "00d017812c73a22ba95198990d79d4ed";
 
 export class WeatherService {
   static getCurrentWeather(city: string): Promise<AxiosResponse<Weather>> {
-    return axios.get(
-      `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
-    );
+    return api.get<Weather>(`/weather?q=${city}`);
   }
 }
